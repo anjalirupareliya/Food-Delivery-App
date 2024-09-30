fea9a6dfd5a0dcac0a47dbfbc7011d8f2bcec1f9
Anjali <rupareliyaanjali529@gmail.com>
2024-09-30 10:52:14 +0530

    first commit

---
+++ src/Components/FoodDisplay/Fooddisplay.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import './Fooddisplay.css'
+import { StoreContext } from '../Context/Storecontext'
+import Fooditem from '../FoodItems/Fooditem'
+
+const Fooddisplay = ({ category }) => {
+    const { food_list } = useContext(StoreContext)
+    return (
+        <div className='food-display' id='food-display'>
+            <h2>Top dishes near for you</h2>
+            <div className='food-display-list'>
+                {food_list.map((item, index) => {
+                    if (category === 'All' || category === item.category) {
+                        return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                    }
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Fooddisplay;
---
+++ src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+      Place Order
+    </div>
+  )
+}
+
+export default PlaceOrder
