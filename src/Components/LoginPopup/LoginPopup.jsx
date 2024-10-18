97267f9b70f69a61c6ced27597d67de772cadd99
Anjali Rupareliya <rupareliyaanjali529@gmail.com>
2024-10-18 17:46:38 +0530

    new changes

--- src/Components/LoginPopup/LoginPopup.jsx
+++ src/Components/LoginPopup/LoginPopup.jsx
@@ -1,16 +1,50 @@
 import React, { useState } from 'react';
 import './LoginPopup.css';
 import { assets } from '../../assets/assets';
+import axios from 'axios';
+import { API_BASE_URL } from "../../constants/apiconstants"
+import { eyeOff } from 'react-icons-kit/feather/eyeOff';
+import { Icon } from 'react-icons-kit';
+import { eye } from 'react-icons-kit/feather/eye'
 
 const LoginPopup = ({ setShowLogin, setUserName }) => {
     const [currState, setCurrState] = useState('Login');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
+    const [confirmpassword, setConfirmPassword] = useState('');
+    const [fullname, setName] = useState('');
+    const [checkboxChecked, setCheckboxChecked] = useState(false);
+    const [errors, setErrors] = useState({});
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+    const [istype, setIsType] = useState('password');
+    const [isicon, setIsIcon] = useState(eyeOff);
 
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
     const [nameError, setNameError] = useState('');
+    const [checkboxError, setCheckboxError] = useState('');
+
+    const handleToggle = () => {
+        if (type === 'password') {
+            setIcon(eye);
+            setType('text')
+        } else {
+            setIcon(eyeOff)
+            setType('password')
+        }
+    }
+
+    const handleToggle1 = () => {
+        if (istype === 'password') {
+            setIsIcon(eye);
+            setIsType('text')
+        } else {
+            setIsIcon(eyeOff);
+            setIsType('password')
+        }
+    }
 
     const validateEmail = () => {
         if (!email) {
@@ -36,12 +70,34 @@ const LoginPopup = ({ setShowLogin, setUserName }) => {
         return true;
     };
 
+    const validateConfirmPassword = () => {
+        if (confirmpassword !== password) {
+            setConfirmPasswordError('Passwords do not match');
+            return false;
+        }
+        setConfirmPasswordError('');
+        return true;
+    };
+
+
     const validateName = () => {
-        if (currState === 'Sign Up' && !name) {
+        if (!fullname) {
             setNameError('Name is required');
+            return false;
+        } else if (!/^[a-zA-Z]+$/.test(fullname)) {
+            setNameError('Please enter a valid name (only letters).');
             return false;
         }
         setNameError('');
+        return true;
+    };
+
+    const validateCheckbox = () => {
+        if (!checkboxChecked) {
+            setCheckboxError('You must agree to the terms and conditions');
+            return false;
+        }
+        setCheckboxError('');
         return true;
     };
 
@@ -66,14 +122,34 @@ const LoginPopup = ({ setShowLogin, setUserName }) => {
         const isEmailValid = validateEmail();
         const isPasswordValid = validatePassword();
         const isNameValid = validateName();
+        const isConfirmPasswordValid = validateConfirmPassword();
+        const isCheckboxValid = validateCheckbox();
 
-        if (isEmailValid && isPasswordValid && isNameValid) {
-            const userData = { name, email, password };
-            localStorage.setItem('user', JSON.stringify(userData));
-            alert('Account created successfully!');
-            setCurrState('Login');
+        if (isEmailValid && isPasswordValid && isConfirmPasswordValid && isNameValid && isCheckboxValid) {
+            const payload = {
+                fullname,
+                email,
+                password,
+                confirmpassword
+            };
+
+            axios.post(API_BASE_URL + '/register', payload)
+                .then(response => {
+                    debugger
+                    if (response.data.status) {
+                        setErrors({ message: response.data.message })
+                    } else {
+                        setErrors({ message1: response.data.message });
+                    }
+                })
+                .catch(error => {
+                    console.error('There was an error registering!', error);
+                    setErrors({ message1: e.response.data })
+                });
         }
+
     };
+
 
     return (
         <div className='login-popup'>
@@ -82,10 +158,17 @@ const LoginPopup = ({ setShowLogin, setUserName }) => {
                     <h2>{currState}</h2>
                     <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt='' />
                 </div>
+                {errors.message1 ? (
+                    <div className="alert alert-danger">{errors.message1}</div>
+                ) : null}
+
+                {errors.message ? (
+                    <div className="alert alert-success">{errors.message}</div>
+                ) : null}
                 <div className='login-popup-inputs'>
                     {currState === 'Sign Up' && (
                         <>
-                            <input type='text' placeholder='Your name' value={name} onChange={(e) => setName(e.target.value)} onBlur={validateName} />
+                            <input type='text' placeholder='Your name' value={fullname} onChange={(e) => setName(e.target.value)} onBlur={validateName} />
                             {nameError && <p className="error-message">{nameError}</p>}
                         </>
                     )}
@@ -95,15 +178,33 @@ const LoginPopup = ({ setShowLogin, setUserName }) => {
 
                     <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} onBlur={validatePassword} />
                     {passwordError && <p className="error-message">{passwordError}</p>}
+
+                    {currState === 'Sign Up' && (
+                        <>
+                            <div>
+                                <input type={type} placeholder='Confirm Password' value={confirmpassword} onChange={(e) => setConfirmPassword(e.target.value)} onBlur={validateConfirmPassword} />
+                                <span onClick={handleToggle1}><Icon icon={isicon} size={18} /></span>
+                                {confirmPasswordError && <p className="error-message">{confirmPasswordError}</p>}
+                            </div>
+                        </>
+                    )}
                 </div>
 
                 <button type='submit'>
                     {currState === 'Sign Up' ? 'Create Account' : 'Login'}
                 </button>
+
                 <div className='login-popup-condition'>
-                    <input type='checkbox' />
-                    <p>By continuing, I agree to the terms of use & privacy policy.</p>
+                    <input
+                        type='checkbox'
+                        checked={checkboxChecked}
+                        onChange={(e) => setCheckboxChecked(e.target.checked)}
+                        onBlur={validateCheckbox}
+                    />
+                    <p className='agree'>By continuing, I agree to the terms of use & privacy policy.</p>
+                    {checkboxError && <p className="error-message">{checkboxError}</p>}
                 </div>
+
                 {currState === "Login" ? (
                     <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
                 ) : (
