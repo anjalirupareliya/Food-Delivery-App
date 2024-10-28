bdccb886a83e733e313848d168cc04bb3cca3d3f
Anjali Rupareliya <rupareliyaanjali529@gmail.com>
2024-10-28 16:45:25 +0530

    working address functionality

--- src/Pages/PlaceOrder/PlaceOrder.jsx
+++ src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -5,6 +5,7 @@ import { StoreContext } from '../../Components/Context/Storecontext';
 import { Link, useNavigate } from 'react-router-dom';
 import { API_BASE_URL } from "../../constants/apiconstants";
 import { AiFillDelete } from "react-icons/ai";
+import { assets } from '../../assets/assets';
 
 const PlaceOrder = () => {
   const { getTotalCartAmount } = useContext(StoreContext);
@@ -26,9 +27,11 @@ const PlaceOrder = () => {
   }
 
   const [formData, setFormData] = useState({
+    id: '',
     firstName: '',
     lastName: '',
     email: '',
+    type: '',
     no: '',
     city: '',
     street: '',
@@ -38,80 +41,93 @@ const PlaceOrder = () => {
     phone: '',
   });
 
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState('');
   const [addressList, setAddressList] = useState([]);
   const [selectedAddressId, setSelectedAddressId] = useState("new_address");
+  const [successMessage, setSuccessMessage] = useState('');
 
   useEffect(() => {
-    const fetchAddressData = async () => {
-      try {
-        const response = await axios.get(`${API_BASE_URL}/address`);
-        if (response.data.status) {
-          setAddressList(response.data.data);
-        }
-      } catch (error) {
-        console.error('Error fetching address data:', error);
-      }
-    };
     fetchAddressData();
   }, []);
+
+  const fetchAddressData = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/address`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      });
+
+      setAddressList(response.data.data);
+    } catch (error) {
+      console.error('Error fetching address data:', error);
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    setErrors({ ...errors, [name]: '' });
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
   };
 
   const handleAddressChange = (addressId) => {
-    const selectedAddress = addressList.find((address) => address.id === addressId);
-    if (selectedAddress) {
-      setSelectedAddressId(addressId);
+    setSelectedAddressId(addressId);
+
+    if (addressId === "new_address") {
       setFormData({
-        firstName: selectedAddress.fullName.split(' ')[0] || '',
-        lastName: selectedAddress.fullName.split(' ')[1] || '',
+        id: '',
+        firstName: '',
+        lastName: '',
         email: '',
-        no: selectedAddress.no || '',
-        city: selectedAddress.city || '',
-        street: selectedAddress.street || '',
-        state: selectedAddress.state || '',
-        zip: selectedAddress.zipCode || '',
-        country: selectedAddress.country || '',
-        phone: selectedAddress.number || '',
+        type: '',
+        no: '',
+        city: '',
+        street: '',
+        state: '',
+        zip: '',
+        country: '',
+        phone: '',
       });
+    }
+    else {
+      const selectedAddress = addressList.find((address) => address.id === addressId);
+      if (selectedAddress) {
+        setFormData({
+          id: selectedAddress.id || '',
+          firstName: selectedAddress.fullName.split(' ')[0] || '',
+          lastName: selectedAddress.fullName.split(' ')[1] || '',
+          email: '',
+          type: selectedAddress.type || '',
+          no: selectedAddress.no || '',
+          city: selectedAddress.city || '',
+          street: selectedAddress.street || '',
+          state: selectedAddress.state || '',
+          zip: selectedAddress.zipCode || '',
+          country: selectedAddress.country || '',
+          phone: selectedAddress.number || '',
+        });
+      }
     }
   };
 
   const handleDeleteAddress = async (id) => {
     try {
-      const token = localStorage.getItem("token");
+      // const token = localStorage.getItem("token");
       const response = await axios.delete(`${API_BASE_URL}/delete/address/${id}`, {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
       if (response.data.status) {
         setAddressList(addressList.filter((address) => address.id !== id));
         if (selectedAddressId === id) {
-          setSelectedAddressId("new_address");
-          setFormData({
-            firstName: '',
-            lastName: '',
-            email: '',
-            no: '',
-            city: '',
-            street: '',
-            state: '',
-            zip: '',
-            country: '',
-            phone: '',
-          });
+          handleAddressChange("new_address");
         }
       }
     } catch (error) {
       console.error("Error deleting address:", error);
     }
   };
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -121,17 +137,13 @@ const PlaceOrder = () => {
   };
 
   const validateForm = () => {
-    const { firstName, lastName, email, no, city, street, state, zip, country, phone } = formData;
+    const { id, firstName, lastName, no, city, street, state, zip, country, phone, type } = formData;
     let newErrors = {};
 
     if (!firstName) newErrors.firstName = 'First name is required';
     if (!lastName) newErrors.lastName = 'Last name is required';
-    if (!email) {
-      newErrors.email = 'Email is required';
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      newErrors.email = 'Email is not valid';
-    }
-    if (!no) newErrors.no = 'House No. is required';
+    if (!type) newErrors.type = 'Please select a type';
+    if (isNaN(no)) newErrors.no = 'House No. must be numberic';
     if (!street) newErrors.street = 'Street is required';
     if (!city) newErrors.city = 'Please select a city';
     if (!state) newErrors.state = 'Please select a state';
@@ -143,21 +155,84 @@ const PlaceOrder = () => {
     return Object.keys(newErrors).length === 0;
   };
 
+  const saveDefaultAddress = async () => {
+    const { id, firstName, lastName, no, street, city, state, zip, country, phone, type } = formData;
+    const addressId = id !== "new_address" ? id : null;
+
+    try {
+      const token = localStorage.getItem("token");
+      const isNewDefault = addressId === "new_address";
+
+      // if (isNewDefault) {
+      //   await axios.patch(`${API_BASE_URL}/address`, {
+      //     isDefault: false,
+      //     user_Id: userId,
+      //   }, {
+      //     headers: {
+      //       Authorization: `Bearer ${token}`,
+      //     },
+      //   });
+      // } else {
+      //   const currentAddress = addressList.find(address => address.id === id);
+      //   if (currentAddress && currentAddress.isDefault) {
+      //     await axios.patch(`${API_BASE_URL}/address`, {
+      //       isDefault: false,
+      //       user_Id: userId,
+      //     }, {
+      //       headers: {
+      //         Authorization: `Bearer ${token}`,
+      //       },
+      //     });
+      //   }
+      // }
+
+      const response = await axios.post(`${API_BASE_URL}/address`, {
+        id,
+        fullName: `${firstName} ${lastName}`,
+        number: phone,
+        no,
+        type,
+        street,
+        city,
+        state,
+        country,
+        zipCode: zip,
+        isDefault: addressId === "new_address" ? true : false,
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.data.status) {
+        setSuccessMessage(response.data.message);
+        fetchAddressData();
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error('Error saving default address:', error);
+    }
+  };
+
+
+  const clearMessage = () => {
+    setSuccessMessage('');
+  };
+
   return (
     <form className='place-order' onSubmit={handleSubmit}>
       <div className='place-order-left'>
-        <p className="title1"> Select a delivery address</p>
+        <p className="title1">Select a delivery address</p>
         {addressList.length > 0 && (
           <div>
             {addressList.map((address) => (
               <div key={address.id} className="radio-container">
-                <div>
-                  <label>
-                    <input type="radio" name="address" className='mr-10' value={address.id} checked={selectedAddressId === address.id} onChange={() => handleAddressChange(address.id)} />
-                    <span className='font-500 pr-5'>{address.fullName}</span>
-                    {` ${address.no}, ${address.street}, ${address.city}, ${address.state} - ${address.zipCode}, ${address.country}`}
-                  </label>
-                </div>
+                <label>
+                  <input type="radio" name="address" className='mr-10' value={address.id} checked={selectedAddressId === address.id} onChange={() => handleAddressChange(address.id)} />
+                  <span className='font-500 pr-5'>{address.fullName}</span>
+                  {` ${address.no}, ${address.street}, ${address.city}, ${address.state} - ${address.zipCode}, ${address.country}`}
+                </label>
                 <div>
                   <AiFillDelete className='cross1' onClick={() => handleDeleteAddress(address.id)} />
                 </div>
@@ -165,14 +240,21 @@ const PlaceOrder = () => {
             ))}
             <div className="radio-container">
               <label>
-                <input type="radio" name="address" className='mr-10' value="new_address" checked={selectedAddressId === "new_address"} onClick={() => setSelectedAddressId("new_address")} />
+                <input type="radio" name="address" className='mr-10' value="new_address" checked={selectedAddressId === "new_address"} onChange={() => handleAddressChange("new_address")} />
                 <span className='font-500 pr-5'>Add new address</span>
               </label>
             </div>
           </div>
         )}
+        <p className='title'>Delivery Information</p><img className="close-icon" onClick={clearMessage} src={assets.cross_icon} alt="close" />
 
-        <p className='title'>Delivery Information</p>
+        {successMessage && (
+          <div >
+            <p>{successMessage}</p>
+            <img className="close-icon" onClick={clearMessage} src={assets.cross_icon} alt="close" />
+          </div>
+        )}
+        <input type='id' name='id' value={formData.id} />
 
         <div className='multi-fields'>
           <div className='form-group w100'>
@@ -190,11 +272,15 @@ const PlaceOrder = () => {
           {errors.phone && <span className='error-message'>{errors.phone}</span>}
         </div>
         <div className='form-group'>
-          <input className='gk' type='email' name='email' placeholder='Email Address' value={formData.email} onChange={handleChange} />
-          {errors.email && <span className='error-message'>{errors.email}</span>}
+          <select id="type" name="type" value={formData.type} onChange={handleChange}>
+            <option value="type">Select Type</option>
+            <option value="Home">Home</option>
+            <option value="Work">Work</option>
+            <option value="Office">Office</option>
+          </select>
         </div>
         <div className='form-group'>
-          <input className='gk' type='text' name='adress' placeholder='Flat, House no., Building, Apartment' value={formData.no} onChange={handleChange} />
+          <input className='gk' type='text' name='no' placeholder='House no.' value={formData.no} onChange={handleChange} />
           {errors.no && <span className='error-message'>{errors.no}</span>}
         </div>
         <div className='form-group'>
@@ -239,9 +325,10 @@ const PlaceOrder = () => {
             {errors.country && <span className='error-message'>{errors.country}</span>}
           </div>
         </div>
-        <button id='tb' type='submit'>Save</button>
+        <button id='tb' type='button' onClick={() => { if (validateForm()) { saveDefaultAddress() } }}>
+          {selectedAddressId === "new_address" ? "Save" : "Update"}
+        </button>
       </div>
-
       <div className='place-order-right'>
         <div className='cart-total'>
           <div className='cart-total-details'>
